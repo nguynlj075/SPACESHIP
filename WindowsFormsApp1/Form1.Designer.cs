89ad10b64fc30e93eb9fd95eb2bde3802e6c40d2
Admin <Admin@ADMIN>
2024-12-16 20:50:40 +0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.LeftMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.DownMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.RightMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.UpMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.MoveMunitionTimer = new System.Windows.Forms.Timer(this.components);
+            this.MoveEnemiesTimer = new System.Windows.Forms.Timer(this.components);
+            this.EXIT = new System.Windows.Forms.Button();
+            this.REPLAY = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.scorelabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Navy;
+            this.Player.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Player.BackgroundImage")));
+            this.Player.Location = new System.Drawing.Point(232, 359);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(104, 100);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Player.TabIndex = 1;
+            this.Player.TabStop = false;
+            this.Player.Click += new System.EventHandler(this.Player_Click);
+            // 
+            // LeftMoveTimer
+            // 
+            this.LeftMoveTimer.Interval = 5;
+            this.LeftMoveTimer.Tick += new System.EventHandler(this.LeftMoveTimer_Tick);
+            // 
+            // DownMoveTimer
+            // 
+            this.DownMoveTimer.Interval = 20;
+            this.DownMoveTimer.Tick += new System.EventHandler(this.DownMoveTimer_Tick);
+            // 
+            // RightMoveTimer
+            // 
+            this.RightMoveTimer.Interval = 5;
+            this.RightMoveTimer.Tick += new System.EventHandler(this.RightMoveTimer_Tick);
+            // 
+            // UpMoveTimer
+            // 
+            this.UpMoveTimer.Interval = 5;
+            this.UpMoveTimer.Tick += new System.EventHandler(this.UpMoveTimer_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // MoveMunitionTimer
+            // 
+            this.MoveMunitionTimer.Enabled = true;
+            this.MoveMunitionTimer.Interval = 40;
+            this.MoveMunitionTimer.Tick += new System.EventHandler(this.MoveMunitionTimer_Tick);
+            // 
+            // MoveEnemiesTimer
+            // 
+            this.MoveEnemiesTimer.Enabled = true;
+            this.MoveEnemiesTimer.Tick += new System.EventHandler(this.MoveEnemiesTimer_Tick);
+            // 
+            // EXIT
+            // 
+            this.EXIT.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.EXIT.Font = new System.Drawing.Font(".VnRevueH", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EXIT.Location = new System.Drawing.Point(195, 244);
+            this.EXIT.Name = "EXIT";
+            this.EXIT.Size = new System.Drawing.Size(188, 52);
+            this.EXIT.TabIndex = 2;
+            this.EXIT.Text = "EXIT";
+            this.EXIT.UseVisualStyleBackColor = true;
+            this.EXIT.Visible = false;
+            this.EXIT.Click += new System.EventHandler(this.EXIT_Click);
+            // 
+            // REPLAY
+            // 
+            this.REPLAY.Font = new System.Drawing.Font(".VnRevueH", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.REPLAY.Location = new System.Drawing.Point(195, 180);
+            this.REPLAY.Name = "REPLAY";
+            this.REPLAY.Size = new System.Drawing.Size(188, 58);
+            this.REPLAY.TabIndex = 3;
+            this.REPLAY.Text = "REPLAY";
+            this.REPLAY.UseVisualStyleBackColor = true;
+            this.REPLAY.Visible = false;
+            this.REPLAY.Click += new System.EventHandler(this.REPLAY_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.BackColor = System.Drawing.Color.Cyan;
+            this.label.Font = new System.Drawing.Font("Haettenschweiler", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(178, 76);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(232, 101);
+            this.label.TabIndex = 4;
+            this.label.Text = "PAUSE";
+            this.label.Visible = false;
+            // 
+            // scorelabel
+            // 
+            this.scorelabel.AutoSize = true;
+            this.scorelabel.Font = new System.Drawing.Font("VNI-Auchon", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorelabel.ForeColor = System.Drawing.Color.Gold;
+            this.scorelabel.Location = new System.Drawing.Point(13, 13);
+            this.scorelabel.Name = "scorelabel";
+            this.scorelabel.Size = new System.Drawing.Size(64, 20);
+            this.scorelabel.TabIndex = 5;
+            this.scorelabel.Text = "Score: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.scorelabel);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.REPLAY);
+            this.Controls.Add(this.EXIT);
+            this.Controls.Add(this.Player);
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer LeftMoveTimer;
+        private System.Windows.Forms.Timer DownMoveTimer;
+        private System.Windows.Forms.Timer RightMoveTimer;
+        private System.Windows.Forms.Timer UpMoveTimer;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer MoveMunitionTimer;
+        private System.Windows.Forms.Timer MoveEnemiesTimer;
+        private System.Windows.Forms.Button EXIT;
+        private System.Windows.Forms.Button REPLAY;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label scorelabel;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[] stars;
+        int backgroundspeed;
+        Random rnd;
+        int playerspeed = 5;
+        int MunitionSpeed = 20;
+        PictureBox[] munitions;
+        Image munition;
+        WindowsMediaPlayer gamemedia;
+        WindowsMediaPlayer shootmedia;
+        WindowsMediaPlayer explosion;
+
+        PictureBox[] enemies;
+        int enemiesspeed;
+
+        //khai báo một số biến cho game
+        int score;
+        int level;
+        int difficulty;
+        bool pause;
+        bool gameisover;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //đầu game sẽ không pause và gameisover
+            pause = false;
+            gameisover = false;
+            //Level và điểm đầu trò chơi:
+            level = 1;
+            score = 0;
+            
+
+            enemiesspeed = 4;
+            // load image
+
+            Image apple = Properties.Resources.apple;
+            Image bag = Properties.Resources.bag;
+            Image can = Properties.Resources.can;
+            enemies = new PictureBox[15];
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                // Khởi tạo PictureBox
+                enemies[i] = new PictureBox();
+
+                // Đặt kích thước cho PictureBox
+                enemies[i].Size = new Size(40, 40);
+
+                // Chế độ hiển thị hình ảnh trong PictureBox
+                enemies[i].SizeMode = PictureBoxSizeMode.Zoom;
+
+                // Loại bỏ đường viền của PictureBox
+                enemies[i].BorderStyle = BorderStyle.None;
+
+                // Đặt thuộc tính Visible của PictureBox thành false, nghĩa là các đối tượng PictureBox không hiển thị ban đầu
+                enemies[i].Visible = false;
+
+                // Thêm PictureBox vào form
+                this.Controls.Add(enemies[i]);
+
+                // Đặt vị trí của PictureBox trên form
+                enemies[i].Location = new Point((i + 1) * 50, -50);
+            }
+            enemies[0].Image = apple;
+            enemies[1].Image = bag;
+            enemies[2].Image = can;
+            try
+            {
+                munition = Image.FromFile(@"C:\assert\munition.png"); // Tải ảnh đạn
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải hình ảnh đạn: " + ex.Message);
+                return;
+            }
+            
+            munitions = new PictureBox[3];
+            for (int i = 0; i < munitions.Length; i++)
+            {
+                munitions[i] = new PictureBox();
+                munitions[i].Size = new Size(8, 8);
+                munitions[i].Image = munition;
+                munitions[i].SizeMode = PictureBoxSizeMode.Zoom;
+                munitions[i].BorderStyle = BorderStyle.None;
+                this.Controls.Add(munitions[i]);
+            }
+            this.Controls.Add(Player);
+            backgroundspeed = 4;
+            stars = new PictureBox[10];
+            rnd = new Random();
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = new PictureBox();
+                stars[i].BorderStyle = BorderStyle.None;
+                stars[i].Location = new Point(rnd.Next(20, 580), rnd.Next(-10, 400));
+
+                if (i % 2 == 1)
+                {
+                    stars[i].Size = new Size(2, 2);
+                    stars[i].BackColor = Color.Wheat;
+                }
+                else
+                {
+                    stars[i].Size = new Size(3, 3);
+                    stars[i].BackColor = Color.DarkGray;
+                }
+
+                this.Controls.Add(stars[i]);
+            }
+            gamemedia = new WindowsMediaPlayer();
+            shootmedia = new WindowsMediaPlayer();
+            explosion = new WindowsMediaPlayer();
+            ///Load all song
+            shootmedia.URL = "C:\\assert\\shoot.mp3";
+            shootmedia.settings.setMode("loop", true);
+            gamemedia.URL = "C:\\assert\\GameSong.mp3";
+            gamemedia.controls.play();
+        }
+ 
+
+        /// <summary>
+        /// Background moving
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < stars.Length / 2; i++)
+            {
+                stars[i].Top += backgroundspeed;
+
+                if (stars[i].Top >= this.Height)
+                {
+                    stars[i].Top = -stars[i].Height;
+                }
+            }
+            for (int i = stars.Length / 2; i < stars.Length; i++)
+            {
+                stars[i].Top += backgroundspeed - 2;
+
+                if (stars[i].Top >= this.Height)
+                {
+                    stars[i].Top = -stars[i].Height;
+                }
+                this.Controls.Add(stars[i]);
+            }
+            
+        }
+
+        private void LeftMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Left > 10)
+            {
+                Player.Left -= playerspeed;
+            }
+        }
+        private void RightMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Right < 580)
+            {
+                Player.Left += playerspeed;
+            }
+        }
+
+        // Hàm di chuyển xuống dưới
+        private void DownMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Top < 400)
+            {
+                Player.Top += playerspeed;
+            }
+        }
+
+        // Hàm di chuyển lên trên
+        private void UpMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Top > 10)
+            {
+                Player.Top -= playerspeed;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!pause)
+            {
+
+                if (e.KeyCode == Keys.Right)
+                {
+                    RightMoveTimer.Start();
+                }
+                if (e.KeyCode == Keys.Left)
+                {
+                    LeftMoveTimer.Start();
+                }
+                if (e.KeyCode == Keys.Down)
+                {
+                    DownMoveTimer.Start();
+                }
+                if (e.KeyCode == Keys.Up)
+                {
+                    UpMoveTimer.Start();
+                }
+            }    
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            RightMoveTimer.Stop();
+            LeftMoveTimer.Stop();
+            DownMoveTimer.Stop();
+            UpMoveTimer.Stop();
+            if (e.KeyCode == Keys.Space) // Kiểm tra phím Space
+            {
+                if (!gameisover) // Kiểm tra xem trò chơi đã kết thúc chưa
+                {
+                    if (pause) // Nếu đang tạm dừng
+                    {
+                        StartTimers(); // Tiếp tục các bộ đếm thời gian
+                        label.Visible = false; // Ẩn nhãn thông báo
+                        gamemedia.controls.play(); // Tiếp tục phát nhạc
+                        pause = false; // Chuyển trạng thái thành không tạm dừng
+                    }
+                    else // Nếu đang chạy
+                    {
+                        // Đặt vị trí nhãn "PAUSED" ra giữa màn hình
+                        label.Location = new Point(this.Width / 2 - 120, 150);
+                        label.Text = "PAUSED"; // Hiển thị chữ "PAUSED"
+                        label.Visible = true; // Hiển thị nhãn
+                        gamemedia.controls.pause(); // Tạm dừng nhạc nền
+                        StopTimers(); // Dừng các bộ đếm thời gian
+                        pause = true; // Chuyển trạng thái thành tạm dừng
+                    }
+                }
+            }
+        }
+
+        private void Player_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MoveMunitionTimer_Tick(object sender, EventArgs e)
+        {
+            shootmedia.controls.play();
+            Collision();
+            for (int i = 0; i < munitions.Length; i++)
+            {
+                if (munitions[i].Top > 0)
+                {
+                    munitions[i].Visible = true;
+                    munitions[i].Top -= MunitionSpeed;
+                }
+                else
+                {
+                    munitions[i].Visible = false;
+                    munitions[i].Location = new Point(Player.Location.X + 20, Player.Location.Y - i * 30);
+                }
+            }
+
+        }
+        /// <summary>
+        /// Di chuyển các đối tượng PictureBox (kẻ địch) xuống dưới màn hình.
+        /// Nếu kẻ địch vượt quá màn hình, vị trí sẽ được đặt lại về phía trên.
+        /// </summary>
+        /// <param name="array">Mảng chứa các PictureBox đại diện cho kẻ địch.</param>
+        /// <param name="speed">Tốc độ di chuyển của kẻ địch.</param>
+        private void MoveEnemies(PictureBox[] array, int speed)
+        {
+            // Duyệt qua từng kẻ địch trong mảng
+            for (int i = 0; i < array.Length; i++)
+            {
+                // Hiển thị kẻ địch (nếu đang bị ẩn)
+                array[i].Visible = true;
+
+                // Di chuyển kẻ địch xuống dưới màn hình theo tốc độ
+                array[i].Top += speed;
+
+                // Kiểm tra nếu kẻ địch đã vượt qua chiều cao của cửa sổ
+                if (array[i].Top > this.Height)
+                {
+                    // Đặt lại vị trí kẻ địch về phía trên màn hình
+                    // Vị trí X cách nhau 50 pixels và Y là -200 (phía trên cửa sổ)
+                    array[i].Location = new Point((i + 1) * 50, -200);
+                }
+            }
+        }
+
+        private void MoveEnemiesTimer_Tick(object sender, EventArgs e)
+        {
+            MoveEnemies(enemies, enemiesspeed);
+        }
+        private void Collision()
+        {
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                if (munitions[0].Bounds.IntersectsWith(enemies[i].Bounds)
+                    || munitions[1].Bounds.IntersectsWith(enemies[i].Bounds)
+                    || munitions[2].Bounds.IntersectsWith(enemies[i].Bounds))
+                {
+                    score += 1;
+                    scorelabel.Text =  $"score: {score}";
+                    explosion.controls.play();
+                    enemies[i].Location = new Point((i + 1) * 50, -100);
+                }
+
+                if (Player.Bounds.IntersectsWith(enemies[i].Bounds))
+                {
+                    explosion.settings.volume = 30;
+                    explosion.controls.play();
+                    Player.Visible = false;
+                    GameOver("Game OVer");
+                }
+            }
+        }
+        private void GameOver(String str)
+        {
+            gamemedia.controls.stop();
+            shootmedia.controls.stop();
+            StopTimers(); // Dừng toàn bộ các Timer
+            EXIT.Visible = true;
+            REPLAY.Visible = true;
+            
+        }
+
+        // Stop Timers
+        private void StopTimers()
+        {
+            timer1.Stop();
+            MoveEnemiesTimer.Stop();
+            MoveMunitionTimer.Stop();
+        }
+
+        // Start Timers
+        private void StartTimers()
+        {
+            timer1.Start();
+            MoveEnemiesTimer.Start();
+            MoveMunitionTimer.Start();
+        }
+
+        private void REPLAY_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            InitializeComponent();
+            Application.Restart();
+        }
+
+        private void EXIT_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+    }
+}
